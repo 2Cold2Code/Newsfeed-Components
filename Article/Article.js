afd40f73de9d7af764f81113cc845c165c869657
Adam Osburn <wizardofosburn@gmail.com>
2020-06-08 21:37:23 -0400

    created articles variable;

--- Article/Article.js
+++ Article/Article.js
@@ -109,3 +109,5 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+const articles = document.querySelector('.articles');    
